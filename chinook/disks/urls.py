1946c847404eafaaa419892b97d8c2e7f61877a0
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-04-15 15:31:23 +0200

    poursuite travail sur les vues non génériques, notament les templates
    Ajout de disks dans chinook.urls.py (oubli)

---
+++ chinook/disks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /disks/
+    path("", views.index, name="index"),
+    # ex: /disks/5/
+    path("<int:id>/", views.detail, name="detail"),
+    # ex: /disks/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Track
+from .models import Album
+from .models import Artist
+
+def index(request):
+    all_album_list = Album.objects.all()
+    template = loader.get_template("disks/index.html")
+    context = {
+        "all_album_list": all_album_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def album(request, album_id):
+    album_track_list = Track.objects.filter(album=album_id)
+    template = loader.get_template("disks/album.html")
+    context = {
+        "all_album_list": all_album_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
